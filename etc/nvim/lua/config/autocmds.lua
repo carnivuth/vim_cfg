13ec19726de48d2db99b8a649cc2394d2f3bab41
Matteo Longhi <mlonghi@jmawireless.com>
2026-05-27 11:06:53 +0200

    added jenkinsfile validation

--- etc/nvim/lua/config/autocmds.lua
+++ etc/nvim/lua/config/autocmds.lua
@@ -2,6 +2,14 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
+
+-- lint jenkinsfiles on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "Jenkinsfile" },
+  callback = function ()
+    require("jenkinsfile_linter").validate()
+  end
+})
 --vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 --  pattern = { "*" },
 --  command = [[normal mfggVG=`f]],
